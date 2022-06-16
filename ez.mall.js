229334d6f8110b4ff7b861a1e2912c8bb55ae951
Rahul Deb <rahulbnc463@gmail.com>
2022-06-16 14:18:36 +0530

    add new lines

---
+++ ez.mall.js
@@ -0,0 +1,31 @@
+let img = document.querySelector(".topdiv>img");
+let buttom = document.querySelectorAll("button");
+let imgNum = 0;
+let imgArr = [
+  "https://cdn3.ezmall.com/0/images/banner/JTA1653977829287/r/1400x730/3_bag_benner.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/FGZ1643028080247/r/1400x730/Block_print_banner_new11.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/JDP1643028197686/r/1400x730/Blue_Pottery_banner_new11.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/UKC1643028284089/r/1400x730/Chikankari_banner_new11.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/FYL1643028385754/r/1400x730/Meenakari_new11.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/VNS1643030634483/r/1400x730/Marble_new11.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/BUP1643027987840/r/1400x730/Agra_Ka_Petha_banner_new11.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/ZAT1653977658926/r/1400x730/1_Banner.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/JYP1647241059608/r/1400x730/Sweets_banner.jpg",
+  "https://cdn3.ezmall.com/0/images/banner/QOL1643128844507/r/1400x730/24pc_Glass_Choodi_banner1.jpg",
+];
+buttom[0].addEventListener("click", function () {
+  if (imgNum === 0) {
+    imgNum = imgArr.length - 1;
+  } else {
+    imgNum--;
+  }
+  img.src = imgArr[imgNum];
+});
+buttom[1].addEventListener("click", function () {
+  if (imgNum === imgArr.length - 1) {
+    imgNum = 0;
+  } else {
+    imgNum++;
+  }
+  img.src = imgArr[imgNum];
+});
